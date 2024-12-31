a559120839226185f28c13ad8aff411f8e020517
tomohiro1024 <gongyezhihong732@gmail.com>
2024-12-31 13:03:31 +0900

    エラー解消

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Report from './pages/Report.tsx';
+import Home from './pages/Home.tsx';
 
 function App() {
   // test
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/report' element={<Report />}/>
+      </Routes>
+    </Router>
   );
 }
 
