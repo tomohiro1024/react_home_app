86aed3225b395c3485e55321aba497d5750671e7
tomohiro1024 <gongyezhihong732@gmail.com>
2025-01-03 12:05:40 +0900

    ルーティング

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,18 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Report from './pages/Report.tsx';
 import Home from './pages/Home.tsx';
 import NoMach from './pages/NoMach';
+import AppLayout from './components/layout/AppLayout';
 
 function App() {
   // test
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/report' element={<Report />}/>
-        <Route path='*' element={<NoMach />}/>
+        <Route path='/' element={<AppLayout />}>
+          <Route index element={<Home />}/>
+          <Route path='/report' element={<Report />}/>
+          <Route path='*' element={<NoMach />}/>
+        </Route>
       </Routes>
     </Router>
   );
