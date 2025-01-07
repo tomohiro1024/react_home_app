a4aaf04fd478f0ac66057b7d918ea0283e72c168
tomohiro1024 <gongyezhihong732@gmail.com>
2025-01-07 10:50:46 +0900

    収入カードの作成

--- src/components/MonthlySummary.tsx
+++ src/components/MonthlySummary.tsx
@@ -1,8 +1,24 @@
+import { Card, CardContent, Grid, Stack, Typography } from "@mui/material";
 import React from 'react'
+import SouthIcon from '@mui/icons-material/South';
+import NorthIcon from '@mui/icons-material/North';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 
 const MonthlySummary = () => {
   return (
-    <div>MonthlySummary</div>
+    <Grid container>
+      <Grid item>
+        <Card sx={{bgcolor: 'blue', color: 'white', borderRadius: '10px'}}>
+          <CardContent>
+            <Stack direction={'row'}>
+              <SouthIcon sx={{fontSize: '2rem'}}/>
+              <Typography>収入</Typography>
+            </Stack>
+            <Typography textAlign={'right'} variant="h5" fontWeight={'700'} sx={{wordBreak: 'break-word'}}>￥1000</Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
   )
 }
 
