82da333893aa0c47432f4a3bf309dd95cea62f6e
tomohiro1024 <gongyezhihong732@gmail.com>
2025-01-09 10:09:52 +0900

    カラー設定

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,4 +1,19 @@
-import { createTheme } from "@mui/material";
+import { createTheme, PaletteColor, PaletteColorOptions } from "@mui/material";
+import { blue, green, red } from "@mui/material/colors";
+
+declare module "@mui/material/styles" {
+  interface Palette {
+    incomeColor: PaletteColor;
+    expenseColor: PaletteColor;
+    balanceColor: PaletteColor;
+  }
+
+  interface PaletteOptions {
+    incomeColor?: PaletteColorOptions;
+    expenseColor?: PaletteColorOptions;
+    balanceColor?: PaletteColorOptions;
+  }
+}
 
 export const theme = createTheme({
   typography: {
@@ -7,4 +22,27 @@ export const theme = createTheme({
     fontWeightMedium: 500, // フォントウェイト500
     fontWeightBold: 700, // フォントウェイト700
   },
+
+  palette: {
+    //収入用
+    incomeColor: {
+      main: blue[500],
+      light: blue[100],
+      dark: blue[700],
+    },
+
+    //支出用
+    expenseColor: {
+      main: red[500],
+      light: red[100],
+      dark: red[700],
+    },
+
+    //残高用
+    balanceColor: {
+      main: green[500],
+      light: green[300],
+      dark: green[700],
+    },
+  }
 })
